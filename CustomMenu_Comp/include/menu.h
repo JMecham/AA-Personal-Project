f499d375d31b5ef235a11fd9b2b1f55e529e614d
JMecham <49049918+JMecham@users.noreply.github.com>
2020-02-19 10:45:25 +0700

    Add files via upload

---
+++ CustomMenu_Comp/include/menu.h
@@ -0,0 +1,44 @@
+#include "vex.h"
+#include "driver.h"
+
+using namespace vex;
+
+void RED1autonMENU(){
+
+}
+
+void RED2autonMENU(){
+  
+}
+
+void RED3autonMENU(){
+  
+}
+
+void RED4autonMENU(){
+  
+}
+
+void BLUE1autonMENU(){
+  
+}
+
+void BLUE2autonMENU(){
+  
+}
+
+void BLUE3autonMENU(){
+  
+}
+
+void BLUE4autonMENU(){
+  
+}
+
+void SKILLS1autonMENU(){
+  
+}
+
+void SKILLS2autonMENU(){
+  
+}
